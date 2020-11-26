71a57a8a471508592ff2b7f3c47b9a5984ffc92b
mattpilla <kaztalek@gmail.com>
2020-11-26 00:13:22 -0600

    more work on the site lol

--- scripts/site/generate-site.js
+++ scripts/site/generate-site.js
@@ -2,6 +2,7 @@ const { promises: fs } = require('fs');
 const path = require('path');
 const sass = require('sass');
 const chalk = require('chalk');
+const md = require('markdown-it')();
 
 const generateCss = async (input, output) => {
     try {
@@ -17,4 +18,51 @@ const generateCss = async (input, output) => {
     }
 };
 
-generateCss(path.resolve(__dirname, './styles.scss'), 'site/styles.css');
+const generateHtml = async (input, output) => {
+    let html;
+    try {
+        // get the html template
+        html = await fs.readFile(input, 'utf8');
+    } catch (e) {
+        return console.error(e);
+    }
+
+    // get and format the command docs based on the command properties
+    let commandFiles;
+    try {
+        commandFiles = await fs.readdir('./commands');
+    } catch (e) {
+        return console.error(e);
+    }
+    const commandHtml = commandFiles.filter(file => file.endsWith('.js'))
+        .map(file => {
+            const command = require(`../../commands/${file}`);
+            // format each bullet
+            const usage = command.usage ? ` ${command.usage}` : ''; // show usage if available
+            let flags = `${command.adminOnly ? 'A' : ''}${command.guildOnly ? 'S' : ''}`;
+            flags = flags ? `**[${flags}]** ` : ''; // list flags if available
+            // list subcommands as indented bullets if they exist
+            const subcommands = command.subcommands ? command.subcommands.map(c => `\n\t- \`${c.name}\`: ${c.description}`).join('') : '';
+            return `<div class="command">
+                <b>.${command.name}${usage}</b>
+                <span>${flags}</span>
+                <span>${md.renderInline(command.description)}</span>
+                ${subcommands}
+            </div>`;
+        })
+        .join('\n');
+
+    // add command docs to html
+    html = html.replace('!!!!', commandHtml);
+
+    // save html
+    try {
+        await fs.writeFile(output, html, 'utf8');
+        console.log(`updated ${chalk.yellow(output)}`);
+    } catch (e) {
+        console.error(e);
+    }
+};
+
+generateCss(path.resolve(__dirname, 'styles.scss'), 'site/styles.css');
+generateHtml(path.resolve(__dirname, 'index-template.html'), 'site/index.html');
